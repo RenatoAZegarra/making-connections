38fe77a0941720d24aab554feb0d6bbe6a7e72d7
RenatoAZegarra <Zegarrarenatoa@gmail.com>
2023-03-21 15:17:47 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeName(element) {
+  element.innerText = "Chad Johnson"
+}
+function deleteName(element){
+  element.remove();
+}
+function addConnection(element,element2){
+  element.innerText = element.innerText-1
+  element2.innerText++;
+}
